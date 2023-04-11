e1dd02835d0c06c5667a75395bba5762126dcfbb
Cyopn <2021452176@teschi.edu.mx>
2023-04-10 20:45:11 -0600

    Controlador
    Limpieza de código (respecto a la versión paralela)

--- handler.js
+++ handler.js
@@ -1,50 +1,60 @@
-const fs = require('fs')
-let command = []
-let alias = []
-const config = require('./config.json')
-const { getAfk, lvlFunc } = require("./lib/functions")
-const temp = require("./media/temp/output.json")
+const fs = require("fs");
+let command = [];
+let alias = [];
+const config = require("./config.json");
 
-fs.readdir('./commands/', (err, files) => {
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  let jsfile = files.filter((f) => f.split(".").pop() === "js");
+  if (jsfile.length <= 0) return console.log("No se encontro ningun comando");
+  jsfile.forEach((f) => {
+    let pull = require(`./commands/${f}`);
+    command.push(pull.config.name);
+    alias.push(pull.config.alias);
+  });
+});
 
-    if (err) return console.error(err)
-    let jsfile = files.filter(f => f.split(".").pop() === "js")
-    if (jsfile.length <= 0) return console.log("No se encontro ningun comando");
-    jsfile.forEach((f) => {
-        let pull = require(`./commands/${f}`)
-        command.push(pull.config.name)
-        alias.push(pull.config.aliases)
-    });
-})
-
-module.exports = msgHandler = async (client, message) => {
-    const { type, id, from, caption, mentionedJidList } = message
-    let { body } = message
-    body = (type === 'chat' && body.startsWith(config.prefix)) ? body : ((type === 'image' && caption || type === 'video' && caption) && caption.startsWith(config.prefix)) ? caption : ''
-
-    try {
-        await lvlFunc(client, message)
-
-        if (mentionedJidList && mentionedJidList[0]) {
-            await getAfk(client, message)
-        }
-        if (body.startsWith(config.prefix)) {
-
-            const args = body.slice(config.prefix.length).trim().split(' ')
-            const comm = args.shift().toLowerCase()
-            const sr = command.indexOf(comm) === -1 ? alias.indexOf(comm) : command.indexOf(comm)
-            if (sr === -1) {
-                return
-            } else {
-                await client.simulateTyping(from, true)
-                const commFil = command[sr]
-                const commFile = require(`./commands/${commFil}`)
-                commFile.run(client, message, args, config)
-            }
-        }
-
-    } catch (e) {
-        console.error(e)
-        await client.reply(from, `Ocurrio un error`, id)
+/**
+ *
+ * @param {Object} message Mensaje recibido
+ * @param {Object} client Cliente (bot)
+ */
+module.exports = async (message, client) => {
+  client.sendSeen(message.id);
+  const { hasQuotedMsg, body } = message;
+  let q;
+  if (hasQuotedMsg) {
+    const qtmp = await message.getQuotedMessage();
+    q = qtmp.body.trim().split(" ");
+  } else {
+    q = undefined;
+  }
+  try {
+    if (body.startsWith(config.prefix)) {
+      const arg = body.slice(config.prefix.length).trim().split(" ");
+      const comm = arg.shift().toLowerCase();
+      const args = [arg, q];
+      const sr =
+        command.indexOf(comm) === -1
+          ? alias.indexOf(comm)
+          : command.indexOf(comm);
+      if (sr === -1) {
+        return;
+      } else {
+        const commFil = command[sr];
+        const commFile = require(`./commands/${commFil}`);
+        commFile.run(message, args);
+      }
     }
-}
+  } catch (e) {
+    console.error(
+      `Error en ${
+        this.config.name
+      } - Hora: ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}:\n`,
+      e.toString()
+    );
+    message.reply(
+      `Ocurrio un problema, si el problema persisite contacta a soporte.`
+    );
+  }
+};
